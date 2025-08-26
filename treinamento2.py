5e8fc1cf6396d99c270d36b8f9c6c6309618e778
Vinicius <vinicios9497@hotmail.com>
2025-08-26 14:31:38 -0300

    mini-calculadora-media

---
+++ treinamento2.py
@@ -0,0 +1,7 @@
+# Uma mini caculadora que te fornece o resultado da média da soma de dois numeros.
+
+n1 = int(input("Digite um numero para somar"))
+n2 = int(input("Digite um outro numero para somar"))
+soma = n1+n2
+divisao = soma/2
+print("A média é de ", divisao)
