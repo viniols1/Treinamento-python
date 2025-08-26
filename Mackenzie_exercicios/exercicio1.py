8e2431065f348d413d855aacf863c675872b8e2c
Vinicius <vinicios9497@hotmail.com>
2025-08-26 20:34:37 -0300

    operacoes-matematicas-numeros-inteiros

---
+++ Mackenzie_exercicios/exercicio1.py
@@ -0,0 +1,16 @@
+# Exercício 1 - Operações matemáticas com dois números inteiros.
+
+n1 = int(input('Digite um numero inteiro: '))
+n2 = int(input('Digite outro numero inteiro: '))
+print('soma:', n1+n2)
+print('Subtração:', n1-n2)
+print('Multiplicação:', n1*n2)
+if n2 != 0:
+    print('Divisão: ', n1/n2)
+    print('Divisão truncada: ', n1//n2)
+    print('Resto:', n1 % n2)
+else:
+    print('Divisão: impossível dividir por zero')
+    print('Divisão truncada: impossível dividir por zero')
+    print('Resto: impossível dividir por zero')
+print('Exponenciação', n1**n2)
