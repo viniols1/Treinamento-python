37d9568dcd5b293a4cdeb5955de7304844814dcc
Vinicius <vinicios9497@hotmail.com>
2025-08-27 14:48:09 -0300

    Add files via upload

---
+++ Mackenzie_exercicios/exercicio2.py
@@ -0,0 +1,7 @@
+# Programa em Python que lê dois números inteiros e exibe o quadrado da diferença do primeiro valor pelo segundo.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+diferenca = n1-n2
+resultado = diferenca ** 2
+print('O quadrado da diferença é: ', resultado)
