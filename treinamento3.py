c596d06315e62b921cd9c1a035b816d8d8733046
Vinicius <vinicios9497@hotmail.com>
2025-08-28 17:20:59 -0300

    Update treinamento3.py

--- treinamento3.py
+++ treinamento3.py
@@ -1,4 +1,4 @@
-# Calculadora básica que calcula os gastos mensais e fornece o valor exato do que sobrou.
+# Calculadora básica que calcula os gastos mensais e fornece o valor que o cliente gastou e o valor que sobrou.
 
 salario = int(input('Digite o valor do seu salário: '))
 aluguel = int(input('Digite o valor do seu alugel: '))
@@ -7,6 +7,9 @@ conta_de_agua = int(input('Digite o valor da sua fatura de energia: '))
 conta_de_luz = int(input('Digite o valor da sua fatura de agua: '))
 internet = int(input('Digite o valor da sua fatura de internet:'))
 conta_de_telefone = int(input('Digite o valor da sua fatura de telefone: '))
+soma = aluguel+compras_de_casa+conta_de_agua + \
+    conta_de_luz+internet+conta_de_telefone
 subtracao = salario-aluguel-compras_de_casa - \
     conta_de_agua-conta_de_luz-internet-conta_de_telefone
-print('Com desconto das despesas o valor que sobra é:', subtracao)
+print('Com base nos calculos, você gastou:', soma)
+print('Com base nos calculos o valor que sobra é:', subtracao)
