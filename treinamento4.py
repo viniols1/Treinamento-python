9c8c94db2a0ef6606612af8e4c05168fee372743
Vinicius <vinicios9497@hotmail.com>
2025-08-28 21:41:06 -0300

    Add files via upload

---
+++ treinamento4.py
@@ -0,0 +1,12 @@
+# Calculadora básica que calcula a média final do aluno.
+
+materia = input('Digite o nome da disciplina: ')
+ndoaluno = input('Digite o nome do aluno: ')
+nmatricula = int(input('Digite o número da matricula do aluno: '))
+nparticipacao = int(input('Digite a nota de participação do aluno: '))
+ntrabalho = int(input('Digite a nota de trabalho do aluno: '))
+ncomportamento = int(input('Digite a nota de comportamento do aluno: '))
+nprova = int(input('Digite a nota da prova do aluno: '))
+soma = nparticipacao+ntrabalho+ncomportamento+nprova
+divisao = soma/4
+print('A média final do aluno é: ', divisao)
