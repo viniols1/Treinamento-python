10c7d839f0b8dbcfa6fe3441817b1be41fdb2a2a
Vinicius <vinicios9497@hotmail.com>
2025-08-31 14:44:55 -0300

    Update exercicio3.py

--- Mackenzie_exercicios/exercicio3.py
+++ Mackenzie_exercicios/exercicio3.py
@@ -6,6 +6,8 @@ premio_total = 780.000
 primeiro = premio_total * 0.46
 segundo = premio_total * 0.32
 terceiro = premio_total - (primeiro + segundo)
+
 print(f"Primeiro colocado receberá: R$ {primeiro:.3f}")
 print(f"Segundo colocado receberá: R$ {segundo:.3f}")
 print(f"Terceiro colocado receberá: R$ {terceiro:.3f}")
+
