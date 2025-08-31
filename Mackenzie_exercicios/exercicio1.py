47ea5cfc4e4326a2a539297d78553b8cc32a31dc
Vinicius <vinicios9497@hotmail.com>
2025-08-31 14:44:20 -0300

    Update exercicio1.py

--- Mackenzie_exercicios/exercicio1.py
+++ Mackenzie_exercicios/exercicio1.py
@@ -2,15 +2,19 @@
 
 n1 = int(input('Digite um numero inteiro: '))
 n2 = int(input('Digite outro numero inteiro: '))
+
 print('soma:', n1+n2)
 print('Subtração:', n1-n2)
 print('Multiplicação:', n1*n2)
+
 if n2 != 0:
     print('Divisão: ', n1/n2)
     print('Divisão truncada: ', n1//n2)
     print('Resto:', n1 % n2)
+
 else:
     print('Divisão: impossível dividir por zero')
     print('Divisão truncada: impossível dividir por zero')
     print('Resto: impossível dividir por zero')
 print('Exponenciação', n1**n2)
+
