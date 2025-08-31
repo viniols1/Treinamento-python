86b2c665a3214f5ee82e50b7f1a5b0bea9a561af
Vinicius <vinicios9497@hotmail.com>
2025-08-31 14:41:20 -0300

    Update treinamento5.py

--- treinamento5.py
+++ treinamento5.py
@@ -14,10 +14,13 @@ set = float(input('Digite o valor do faturamento de Setembro: '))
 out = float(input('Digite o valor do faturamento de Outubro: '))
 nov = float(input('Digite o valor do faturamento de Novembro: '))
 dez = float(input('Digite o valor do faturamento de Dezembro: '))
+
 soma = jan+fev+mar+abr+mai+jun+jul+ago+set+out+nov+dez
 divisao = soma/12
+
 print(f'A soma anual de faturamento é R$: {soma:,.2f}')
 print(f'A média mensal de faturamento é R$: {divisao:,.2f}')
+
 
 
 
