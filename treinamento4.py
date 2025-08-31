43651131b25aa9f7a9b591029be5e67253d350f5
Vinicius <vinicios9497@hotmail.com>
2025-08-31 14:41:44 -0300

    Update treinamento4.py

--- treinamento4.py
+++ treinamento4.py
@@ -7,6 +7,9 @@ nparticipacao = int(input('Digite a nota de participação do aluno: '))
 ntrabalho = int(input('Digite a nota de trabalho do aluno: '))
 ncomportamento = int(input('Digite a nota de comportamento do aluno: '))
 nprova = int(input('Digite a nota da prova do aluno: '))
+
 soma = nparticipacao+ntrabalho+ncomportamento+nprova
 divisao = soma/4
+
 print('A média final do aluno é: ', divisao)
+
