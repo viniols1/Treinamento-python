568a4cad77d9f7675dc5f43d09a23d02354be734
Vinicius <vinicios9497@hotmail.com>
2025-08-31 14:42:07 -0300

    Update treinamento3.py

--- treinamento3.py
+++ treinamento3.py
@@ -7,9 +7,12 @@ conta_de_agua = int(input('Digite o valor da sua fatura de energia: '))
 conta_de_luz = int(input('Digite o valor da sua fatura de agua: '))
 internet = int(input('Digite o valor da sua fatura de internet:'))
 conta_de_telefone = int(input('Digite o valor da sua fatura de telefone: '))
+
 soma = aluguel+compras_de_casa+conta_de_agua + \
     conta_de_luz+internet+conta_de_telefone
 subtracao = salario-aluguel-compras_de_casa - \
     conta_de_agua-conta_de_luz-internet-conta_de_telefone
+
 print('Com base nos calculos, você gastou:', soma)
 print('Com base nos calculos o valor que sobra é:', subtracao)
+
