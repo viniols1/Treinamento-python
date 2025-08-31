066f2bbdf4368aad60b1876d61ebbe6b292c7239
Vinicius <vinicios9497@hotmail.com>
2025-08-31 14:42:47 -0300

    Update treinamento.py

--- treinamento.py
+++ treinamento.py
@@ -2,14 +2,17 @@
 
 n1 = int(input("Digite um numero"))
 n2 = int(input("Digite outro numero"))
+
 soma = n1+n2
 divisao = n1/n2
 multiplicacao = n1*n2
 subitracao = n1-n2
+
 print("A soma é igual a ", soma)
 print("A multiplicação é igual a ", multiplicacao)
 print("A divisão é igual a ", divisao)
 print("A subtração é igual a", subitracao)
+
 
 
 
